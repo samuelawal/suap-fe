f44baaab9e9dbfc6a5cbc30ba7f79b1055574252
Samuel Lawal <samuellawal60@gmail.com>
2023-09-05 22:42:01 +0100

    chore: Made changes to ui page

--- src/pages/Respond.js
+++ src/pages/Respond.js
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
+// import RespondImg from './responder.png'
 
 const Respond = () => {
+  const responder = process.env.PUBLIC_URL + '/responder.png';
+  const booster = process.env.PUBLIC_URL + '/booster.png';
+  const amico = process.env.PUBLIC_URL + '/amico.png';
     const [randomObject, setRandomObject] = useState(null);
     useEffect(() => {
     const respondsData = [{
         intro: 'You are a',
         name: 'Responder',
+        image: responder,
         text: 'You’re interested in sustainability and are still learning about it. You like the extra income you will receive from selling Pre-owned items. '
     },
         {
             intro: 'You are an',
             name: 'Eco-Boosters',
+            image: booster,
             text: 'You give a lot of importance to sustainability in your life choices. You also seek out eco-friendly products and services. You are an amazing addition to the Suap community.'        
     }, 
         {
             intro: 'You are an',
             name: 'Eco-Warrior',
+            image: amico,
             text: 'You already sell your pre-owned items to your community. You are eco-conscious also. You would be a great ambassador for Suap'  ,
     }
 ]
@@ -34,7 +41,7 @@ const getRandomItem = () => {
         {/* <Navbar/> */}
         <section className='container px-8 mx-auto'>
             <div className='mx-auto md:w-[600px] w-full'>
-            <div>  <img src='./responder.png' alt='responder' className='mx-auto' /></div>
+            <div>  <img src={randomObject?.image} alt='responder' className='mx-auto '/></div>
             <h2 className='font-black text-center mt-4 md:text-5xl text-3xl'>
             {randomObject && (
               <span>
@@ -43,7 +50,7 @@ const getRandomItem = () => {
               </span>
             )}{' '}
             <span>
-              <img src='./thumbs.png' className='inline' alt='thumbs' />
+              <img src='./thumbs.png' className='inline md:w-20 w-12' alt='thumbs' />
             </span>
           </h2>
           {randomObject && <p className='mt-5 text-center'>{randomObject.text}</p>}
