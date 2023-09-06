14f050ca7b89756de976b4078124179cd9c09a4b
Samuel Lawal <samuellawal60@gmail.com>
2023-09-06 22:49:02 +0100

    fix: Fixed icon issue on live site

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -19,23 +19,10 @@ const HomeSection = () => {
         />
         </span><br/> items for cash
     </h1>
-    <p className='pt-12 md:text-4xl text-2xl font-bold text-[#247291]'>Never loose value with Suap</p>
+    <p className='pt-12 md:text-4xl text-2xl font-bold text-[#247291]'>Never lose value with Suap</p>
     <div className='mt-24'>
     <Link to="https://docs.google.com/forms/d/e/1FAIpQLScKJjMf47FsTZIiDqs_0C5XhzMpQFrHiWBJ1V-FlUP3mz5vHQ/viewform?usp=sf_link"> <button className='button rounded-md font-bold text-2xl p-4'>Get Early Access</button></Link>
     </div>
-    {/* <div className='mt-28 '>
-        <div className='grid grid-cols-3 place-items-center gap-8 relative'>
-            <div className='p-8 text-4xl rounded-md bg-white  w-60 text-center'>
-                Keep
-            </div>
-            <div className='p-8 text-4xl rounded-md bg-white  w-60 text-center'>
-                Suap
-            </div>
-            <div className='p-8 text-4xl rounded-md bg-white  w-60 text-center'>
-                Recycle
-            </div>
-        </div>
-    </div>   */}
     </div>
  </section>
   )
